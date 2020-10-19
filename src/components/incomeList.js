c0b0aac77b8ac48f96577cfc10d8ae3404b543aa
uzairkorai <uzairahmed097@gmail.com>
2020-10-19 18:50:14 +0500

    first

---
+++ src/components/incomeList.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import IncomeItem from './incomeItem'
+
+function incomeList({ income, setIncome }) {
+    const removeIncome = (i) => {
+        let temp = income.filter((v, index) => index != i)
+        setIncome(temp)
+    }
+
+    const sortByDate = (a, b) => {
+        return a.date - b.date
+    }
+
+    return ( <
+        div className = 'income-list' > {
+            income.sort(sortByDate).map((value, index) => ( <
+                IncomeItem key = { index }
+                income = { value }
+                index = { index }
+                removeIncome = { removeIncome }
+                />
+            ))
+        } <
+        /div>
+    )
+}
+
+export default incomeList
